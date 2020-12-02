bf6eec09f6b4e62b9017a96361a50fe954b3ae8a
Prp <mohannadyoussef93@gmail.com>
2020-12-02 17:07:25 +0200

    translate

---
+++ lib/providers/book_property_provider.dart
@@ -0,0 +1,71 @@
+import 'package:aqar_bazar/Network/Api.dart';
+import 'package:aqar_bazar/Utils/session_manager.dart';
+import 'package:aqar_bazar/models/cancel_request_response.dart';
+import 'package:aqar_bazar/models/succes_string_response.dart';
+import 'package:aqar_bazar/screens/Auth/models/log_in_response.dart';
+import 'package:aqar_bazar/screens/profile/models/user_profile_model.dart';
+import 'package:flutter/foundation.dart';
+
+import 'package:provider/provider.dart';
+
+class BookPropertyProvider with ChangeNotifier {
+  bool loading = false;
+
+  Api api = Api();
+
+  bool requested= false;
+  SuccessStringResponse succressResponse;
+
+  SessionManager sessionManager = SessionManager();
+
+
+  String message;
+
+  Future<SuccessStringResponse> bookProperty(String id ,String from , String to , String amount,String sellType) async {
+    setLoading(true);
+    try {
+      String token = await sessionManager.getAuthToken();
+      String cookie = await sessionManager.getSessionToken();
+
+      succressResponse = await api.bookProperty(token,id,cookie,from,to,amount,sellType);
+
+
+      if (succressResponse != null) {
+
+        setRequested(true);
+
+        setLoading(false);
+        return succressResponse;
+      }
+
+
+      setLoading(false);
+      return null;
+    } catch (e) {
+
+      setLoading(false);
+      return null;
+    }
+  }
+
+
+
+  void setLoading(bool value) {
+    loading = value;
+    notifyListeners();
+  }
+
+  bool isLoading() {
+    return loading;
+  }
+
+
+  void setRequested(bool value) {
+    requested = value;
+    notifyListeners();
+  }
+
+  bool isRequested() {
+    return requested;
+  }
+}
